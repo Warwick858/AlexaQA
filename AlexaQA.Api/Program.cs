5f2c971ce3e98c1a6f42c086d94ae24256c6e332
Warwick858 <lo40ops@gmail.com>
2020-03-07 11:32:07 -0700

    Add project files.

---
+++ AlexaQA.Api/Program.cs
@@ -0,0 +1,66 @@
+﻿// ******************************************************************************************************************
+//  This file is part of AlexaQA.
+//
+//  AlexaQA - web service for testing Alexa related functionality.
+//  Copyright(C)  2020  James LoForti
+//  Contact Info: jamesloforti@gmail.com
+//
+//  AlexaQA is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.If not, see<https://www.gnu.org/licenses/>.
+//									     ____.           .____             _____  _______   
+//									    |    |           |    |    ____   /  |  | \   _  \  
+//									    |    |   ______  |    |   /  _ \ /   |  |_/  /_\  \ 
+//									/\__|    |  /_____/  |    |__(  <_> )    ^   /\  \_/   \
+//									\________|           |_______ \____/\____   |  \_____  /
+//									                             \/          |__|        \/ 
+//
+// ******************************************************************************************************************
+//
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using System;
+
+namespace AlexaQA.Api
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+						.MinimumLevel.Information()
+						.WriteTo.File("logs\\log.txt",
+							rollingInterval: RollingInterval.Day,
+							rollOnFileSizeLimit: true)
+						.CreateLogger();
+
+			try
+			{
+				Log.Information("Starting web host...");
+				CreateWebHostBuilder(args).Build().Run();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Host terminated unexpectedly.");
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		} // end method
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
+	} // end class
+} // end namespace
---
+++ README.md
@@ -0,0 +1,15 @@
+![JRL Logo](http://jimmyloforti.com/_common/images/jrl_logo2.png)
+
+## Application Name: AlexaQA ##
+
+* Framework: .NET Core 3.1
+
+* Solution Type(s): .NET Web, Class Library
+
+* Description: web service for testing Alexa related functionality.
+
+* Nuget Packages:
+	* Serilog - 2.9.1-dev-01154
+	* Serilog.Sinks.File - 4.1.0
+	* Swashbuckle.AspNetCore - 5.0.0
+	* Swashbuckle.AspNetCore.Annotations - 5.0.0
